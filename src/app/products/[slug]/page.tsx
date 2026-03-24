5ae8613d394c939e2b43b04b6ace1e2f2fdf5fed
HARSH GAMI <harshgami@HARSHs-MacBook-Air.local>
2026-03-24 23:24:44 +0530

    build error fix and env updated

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,10 +1,15 @@
 import CommanLayout from "@/components/CommanLayout";
 import ProductDetailClient from "./_components/ProductDetailClient";
 
-export default function ProductDetailPage({ params }: { params: { slug: string } }) {
+export default async function ProductDetailPage({
+    params,
+}: {
+    params: Promise<{ slug: string }>;
+}) {
+    const { slug } = await params;
     return (
         <CommanLayout>
-            <ProductDetailClient slug={params.slug} />
+            <ProductDetailClient slug={slug} />
         </CommanLayout>
     );
 }
